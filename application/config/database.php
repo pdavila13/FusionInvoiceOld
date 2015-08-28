e0af558878bbdc84c69ac6e7c2819934ce499038
Paolo Dávila <pdavila@iesebre.com>
2015-08-28 18:11:56 +0200

    ok

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'passwd';
+$db['default']['password'] = 'HOLA';
 $db['default']['database'] = 'fusioninvoice';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
